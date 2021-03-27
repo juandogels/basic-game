954758c96d61ae50729730ccfaa1a0cbcc441d13
Suwandre <suwandresukijat@gmail.com>
2021-03-27 21:29:06 +0100

    added GameManager script that ends game and restarts the level

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     public PlayerMovement movement;
+    
    void OnCollisionEnter(Collision collisionInfo) {
        if(collisionInfo.collider.tag == "Obstacle") {
-           movement.enabled = false;
+           GetComponent<PlayerMovement>().enabled = false;
+           FindObjectOfType<GameManager>().EndGame();
        }
-
    }
-    
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,6 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rb;
     public float forwardForce  = 2000f;
     public float sidewaysForce = 500f;
-    // Start is called before the first frame update
-    
-
-    
     void FixedUpdate()
     {
         //adding forward force
@@ -20,6 +16,9 @@ public class PlayerMovement : MonoBehaviour
         }
         if (Input.GetKey("a")) {
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (rb.position.y < -1f) {
+            FindObjectOfType<GameManager>().EndGame();
         }
     }
 }
