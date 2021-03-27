c59a4ef23fa892ecb06a3aa2e8e7dad9fcbcbf8e
Suwandre <suwandresukijat@gmail.com>
2021-03-27 21:07:54 +0100

    Added entire Unity folder, assets, scripts, prefabs, materials
    Created the first set of assets, first level (scene), material and scripts.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce  = 2000f;
+    public float sidewaysForce = 500f;
+    // Start is called before the first frame update
+    
+
+    
+    void FixedUpdate()
+    {
+        //adding forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d")) {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a")) {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
